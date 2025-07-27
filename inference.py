a80727614d772d09da2e83429266667df99055f8
faizaladin <faizaladin365@gmail.com>
2025-07-27 18:11:39 -0400

    new prompt

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Load the model in half-precision
 model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf", torch_dtype=torch.float16, device_map="auto")
 processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
-image = Image.open("success_rain.png")
+image = Image.open("paired_fail_4k.png")
 
 
 conversation = [
@@ -13,7 +13,7 @@ conversation = [
         "role": "user",
         "content": [
             {"type": "image", "image": image},
-            {"type": "text", "text": "This is an image taken by a car using a vision based algorithm to steer. Is there a cause of failure in this image, and if so what is the cause of failure? Please give a yes or no answer followed by reasoning specific to the image and pertains to the weather condition. Create a list of these failures and provide bullet points of reasoning for each one. Lastly, see if there are similar failures that could arise in different weather conditions."},
+            {"type": "text", "text": "This is a paired image of two cars using a vision based algorithm to steer under different weather conditions. Is there a cause of failure in this image, and if so what is the cause of failure? Please give a yes or no answer followed by reasoning specific to the image and pertains to the weather condition. Create a list of these failures and provide bullet points of reasoning for each one. Lastly, see if there are similar failures that could arise in different weather conditions."},
         ],
     },
 ]
@@ -27,6 +27,6 @@ inputs = processor.apply_chat_template(
 ).to(model.device, torch.float16)
 
 # Generate
-generate_ids = model.generate(**inputs, max_new_tokens=100)
+generate_ids = model.generate(**inputs, max_new_tokens=1000)
 output = processor.batch_decode(generate_ids, skip_special_tokens=True)
 print(output)
