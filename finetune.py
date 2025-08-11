294b5194ad8585192218743ab318362c5572b62c
faizaladin <faizaladin365@gmail.com>
2025-08-10 22:28:48 -0700

    test

--- finetune.py
+++ finetune.py
@@ -187,9 +187,9 @@ if __name__ == "__main__":
     # --- 4. Training Arguments ---
     training_args = TrainingArguments(
         output_dir="llava-finetuned-model-weighted",
-        per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
-        gradient_accumulation_steps=8,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
+        gradient_accumulation_steps=16,
         num_train_epochs=100,
         learning_rate=1e-5,
         weight_decay=0.01,
